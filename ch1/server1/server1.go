0049e392772cb21de63cb529494b705feccdea09
Gaganpreet <me@gaganpreet.in>
2020-05-04 00:33:35 +0200

    Echo handler

--- ch1/server1/server1.go
+++ ch1/server1/server1.go
@@ -13,6 +13,7 @@ var count int
 func main() {
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/count", counter)
+	http.HandleFunc("/echo", echo)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
@@ -28,4 +29,22 @@ func counter(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Count: %d", count)
 	mu.Unlock()
 
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
 }
