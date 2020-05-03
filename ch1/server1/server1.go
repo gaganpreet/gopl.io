48895b358e9e1e0b33ae812a64fc06d881bb8be3
Gaganpreet <me@gaganpreet.in>
2020-05-04 00:28:06 +0200

    Add counter handler

--- ch1/server1/server1.go
+++ ch1/server1/server1.go
@@ -4,13 +4,28 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 )
+
+var mu sync.Mutex
+var count int
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count += 1
 	fmt.Fprintf(w, "Hello, world! Request to %q", r.URL.Path)
+	mu.Unlock()
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count: %d", count)
+	mu.Unlock()
+
 }
