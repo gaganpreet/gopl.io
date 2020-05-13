363d382168f203e46c3e32786097214fc71cfbf5
Gaganpreet <me@gaganpreet.in>
2020-05-13 22:57:21 +0200

    basename1

---
+++ ch4/basename1/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func basename1(s string) string {
+	// Discard last '/' and everything before
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '/' {
+			s = s[i+1:]
+			break
+		}
+	}
+	// Preserve everything before last '.'
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '.' {
+			s = s[:i]
+			break
+		}
+	}
+	return s
+}
+
+func main() {
+	fmt.Println(basename1("a/b/c.go"))
+}
