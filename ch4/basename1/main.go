34a5da5bcb6302ddba8781922a1694be37d11f68
Gaganpreet <me@gaganpreet.in>
2020-05-13 22:59:07 +0200

    basename2

--- ch4/basename1/main.go
+++ ch4/basename1/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func basename1(s string) string {
 	// Discard last '/' and everything before
@@ -20,6 +23,15 @@ func basename1(s string) string {
 	return s
 }
 
+func basename2(s string) string {
+	slash := strings.LastIndex(s, "/") // -1 if "/" not found
+	s = s[slash+1:]
+	if dot := strings.LastIndex(s, "."); dot >= 0 {
+		s = s[:dot]
+	}
+	return s
+}
+
 func main() {
-	fmt.Println(basename1("a/b/c.go"))
+	fmt.Println(basename2("a/b/c.go"))
 }
