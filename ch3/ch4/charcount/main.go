e3bf456d71331b91c7cca49ffef29810cecbec06
Gaganpreet <me@gaganpreet.in>
2020-06-01 18:35:24 +0200

    character type frequency exercise 4.8

--- ch3/ch4/charcount/main.go
+++ ch3/ch4/charcount/main.go
@@ -9,15 +9,36 @@ import (
 	"unicode/utf8"
 )
 
+type RuneType string
+
+const (
+	RuneLetter RuneType = "letter"
+	RuneNumber RuneType = "number"
+	RuneSpace  RuneType = "space"
+	RuneOther  RuneType = "other"
+)
+
 func main() {
-	counts := make(map[rune]int)    // counts of Unicode characters
-	var utflen [utf8.UTFMax + 1]int // count of lengths of UTF-8 encodings
+	counts := make(map[rune]int)         // counts of Unicode characters
+	typeCounts := make(map[RuneType]int) // counts of rune types
+	var utflen [utf8.UTFMax + 1]int      // count of lengths of UTF-8 encodings
 	invalid := 0
 
 	in := bufio.NewReader(os.Stdin)
 
 	for {
 		r, n, err := in.ReadRune() // returns rune, nbytes, error
+		var runeType RuneType
+		if unicode.IsLetter(r) {
+			runeType = RuneLetter
+		} else if unicode.IsNumber(r) {
+			runeType = RuneNumber
+		} else if unicode.IsSpace(r) {
+			runeType = RuneSpace
+		} else {
+			runeType = RuneOther
+		}
+		typeCounts[runeType] += 1
 		if err == io.EOF {
 			break
 		}
@@ -41,6 +62,10 @@ func main() {
 		if i > 0 {
 			fmt.Printf("%d\t%d\n", i, n)
 		}
+	}
+	fmt.Print("\ntype\tcount\n")
+	for i, n := range typeCounts {
+		fmt.Printf("%s\t%d\n", i, n)
 	}
 	if invalid > 0 {
 		fmt.Printf("\n%d invalid UTF-8 characters\n", invalid)
