18588fcaaf5e75cea870a5d9c45a28bd739ecc80
Gaganpreet <me@gaganpreet.in>
2020-06-07 21:14:51 +0200

    exercise 5.1, visit siblings recursively

--- ch5/findlinks1/main.go
+++ ch5/findlinks1/main.go
@@ -7,19 +7,36 @@ import (
 	"golang.org/x/net/html"
 )
 
-func visit(links []string, n *html.Node) [] string {
+func visit(links []string, n *html.Node) []string {
 	if n.Type == html.ElementNode && n.Data == "a" {
 		for _, a := range n.Attr {
 			if a.Key == "href" {
 				links = append(links, a.Val)
-				
+
 			}
-			
+
 		}
 	}
-	for c := n.FirstChild; c != nil;  c = c.NextSibling {
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
 		links = visit(links, c)
 	}
+	return links
+}
+
+func visitRecursively(links []string, n *html.Node) []string {
+	// change the findlinks program to traverse the n.FirstChild linked list using recursive calls to visit instead of a loop
+	if n == nil {
+		return links
+	}
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+	links = visitRecursively(links, n.NextSibling)
+	links = visitRecursively(links, n.FirstChild)
 	return links
 }
 
@@ -29,7 +46,7 @@ func main() {
 		fmt.Fprintf(os.Stderr, "findlink1: %v\n", err)
 		os.Exit(1)
 	}
-	for _, link  := range visit(nil, doc) {
+	for _, link := range visitRecursively(nil, doc) {
 		fmt.Println(link)
 	}
 }
