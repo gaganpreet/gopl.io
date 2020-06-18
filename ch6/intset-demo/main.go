b52307da08bfc96cd7539f951072ea636b85ccb5
Gaganpreet <me@gaganpreet.in>
2020-06-18 20:48:58 +0200

    intset exercise 6.1 & 6.2

--- ch6/intset-demo/main.go
+++ ch6/intset-demo/main.go
@@ -8,20 +8,27 @@ import (
 
 func main() {
 	var x, y intset.IntSet
-	x.Add(1)
-	x.Add(144)
-	x.Add(9)
+	x.AddAll(1, 144, 9)
 	fmt.Println(x.String())
+	fmt.Println(x.Length())
 
-	y.Add(9)
-	y.Add(42)
+	y.AddAll(9, 42)
+
+	fmt.Println(x.Remove(144))
+	fmt.Println(x.Remove(5))
 
 	fmt.Println(y.String())
-
+	fmt.Println(x.Length())
+	
 	x.UnionWith(&y)
-	fmt.Println(x.String())
-	fmt.Println(x.Has(9), x.Has(123))
 
 	fmt.Println(&x)
-	fmt.Println(&y)
+	t := *x.Copy()
+	fmt.Println(&x)
+	fmt.Println(&t)
+
+	x.Clear()
+	fmt.Println(&x)
+	x.Add(10)
+	fmt.Println(&x)
 }
--- ch6/intset/main.go
+++ ch6/intset/main.go
@@ -9,12 +9,12 @@ type IntSet struct {
 	words []uint64
 }
 
-func (s *IntSet) Has (x int) bool {
+func (s *IntSet) Has(x int) bool {
 	word, bit := x/64, uint(x%64)
-	return word < len(s.words) && s.words[word] & (1<<bit) != 0
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
 }
 
-func (s *IntSet) Add (x int) {
+func (s *IntSet) Add(x int) {
 	word, bit := x/64, uint(x%64)
 	for word >= len(s.words) {
 		s.words = append(s.words, 0)
@@ -22,7 +22,26 @@ func (s *IntSet) Add (x int) {
 	s.words[word] |= 1 << bit
 }
 
-func (s *IntSet) UnionWith (t *IntSet) {
+func (s *IntSet) AddAll(numbers ...int) {
+	for _, i := range numbers {
+		s.Add(i)
+	}
+}
+
+func (s *IntSet) Remove(x int) bool {
+	if !s.Has(x) {
+		return false
+	}
+	word, bit := x / 64, uint(x%64)
+	s.words[word] &= ^(1<<bit)
+	return true
+}
+
+func (s *IntSet) Clear() {
+	s.words = nil
+}
+
+func (s *IntSet) UnionWith(t *IntSet) {
 	for i, tword := range t.words {
 		if i < len(s.words) {
 			s.words[i] |= tword
@@ -34,21 +53,44 @@ func (s *IntSet) UnionWith (t *IntSet) {
 
 func (s *IntSet) String() string {
 	var buf bytes.Buffer
-	fmt.Println("here")
 	buf.WriteByte('{')
 	for i, word := range s.words {
 		if word == 0 {
 			continue
 		}
 		for j := 0; j < 64; j++ {
-			if word &(1<<uint(j)) != 0 {
+			if word&(1<<uint(j)) != 0 {
 				if buf.Len() > len("{") {
 					buf.WriteByte(' ')
 				}
-				fmt.Fprintf(&buf, "%d", 64*i +j)
+				fmt.Fprintf(&buf, "%d", 64*i+j)
 			}
 		}
 	}
 	buf.WriteByte('}')
 	return buf.String()
+}
+
+func (s *IntSet) Length() int64 {
+	var length int64
+	for _, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				length++
+			}
+
+		}
+	}
+	return length
+}
+
+func (s *IntSet) Copy() *IntSet {
+	var p IntSet
+	for _, word := range s.words {
+		p.words = append(p.words, word)
+	}
+	return &p
 }
