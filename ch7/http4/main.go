1be04ecbce34e97eb241fb1e23bb6e83e352acc6
Gaganpreet <gaganpreet.arora@gmail.com>
2020-09-09 21:22:06 +0200

    http4

---
+++ ch7/http4/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	db := database{"shoes": 50, "socks": 5}
+	http.HandleFunc("/list", db.list)
+	http.HandleFunc("/price", db.price)
+	/*
+	mux := http.NewServeMux()
+	// http3a
+	mux.HandleFunc("/list", db.list)
+	mux.HandleFunc("/price", db.price)
+	*/
+	/* http3
+	//mux.Handle("/list", http.HandlerFunc(db.list))
+	//mux.Handle("/price", http.HandlerFunc(db.price))
+	*/
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+type database map[string]dollars
+
+type dollars float32
+
+func (d dollars) String() string {
+	return fmt.Sprintf("$%.2f", d)
+}
+
+func (db database) list(w http.ResponseWriter, req *http.Request) {
+	for item, price := range db {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+
+func (db database) price(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+	fmt.Fprintf(w, "%s\n", price)
+}
