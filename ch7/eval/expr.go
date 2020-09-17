ed50f161789e37a168cfec8fd1488ef806b55739
Gaganpreet Arora <me@gaganpreet.in>
2020-09-17 22:05:38 +0200

    add Check

--- ch7/eval/expr.go
+++ ch7/eval/expr.go
@@ -3,13 +3,15 @@ package eval
 import (
 	"fmt"
 	"math"
+	"strings"
 )
 
 // An arithmetic expression
-type Expr interface{
+type Expr interface {
 	Eval(env Env) float64
+	// Check reports errors in this Expr and adds its Vars to the set.
+	Check(vars map[Var]bool) error
 }
-
 
 // A Var identifies a variable, eg: x
 type Var string
@@ -20,18 +22,18 @@ type literal float64
 // A unary represents a unary operator expression, eg, -x
 type unary struct {
 	op rune // one of + or -
-	x Expr
+	x  Expr
 }
 
 // A binary represents a binary operator expression eg x + y
 type binary struct {
-	op rune // one of +, -, * or /
+	op   rune // one of +, -, * or /
 	x, y Expr
 }
 
 // A call represents a function call express eg: sin(x)
 type call struct {
-	fn string // one of pow, sin, sqrt
+	fn   string // one of pow, sin, sqrt
 	args []Expr
 }
 
@@ -42,8 +44,52 @@ func (v Var) Eval(env Env) float64 {
 	return env[v]
 }
 
+func (v Var) Check(vars map[Var]bool) error {
+	vars[v] = true
+	return nil
+}
+
+func (literal) Check(vars map[Var]bool) error {
+	return nil
+}
+
 func (l literal) Eval(_ Env) float64 {
 	return float64(l)
+}
+
+func (u unary) Check(vars map[Var]bool) error {
+	if !strings.ContainsRune("+-", u.op) {
+		return fmt.Errorf("unexpected unary op %q", u.op)
+	}
+	return u.x.Check(vars)
+}
+
+func (b binary) Check(vars map[Var]bool) error {
+	if !strings.ContainsRune("+-*/", b.op) {
+		return fmt.Errorf("unexpected binary op %q", b.op)
+	}
+	if err := b.x.Check(vars); err != nil {
+		return err
+	}
+	return b.y.Check(vars)
+}
+
+var numParams = map[string]int{"pow": 2, "sin": 1, "sqrt": 1}
+
+func (c call) Check(vars map[Var]bool) error {
+	arity, ok := numParams[c.fn]
+	if !ok {
+		return fmt.Errorf("unknown function %q", c.fn)
+	}
+	if len(c.args) != arity {
+		return fmt.Errorf("call to %s has %d args, want %d", c.fn, len(c.args), arity)
+	}
+	for _, arg := range c.args {
+		if err := arg.Check(vars); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func (u unary) Eval(env Env) float64 {
@@ -70,7 +116,7 @@ func (b binary) Eval(env Env) float64 {
 	panic(fmt.Sprintf("unsupported binary operator: %q", b.op))
 }
 
-func (c call) Eval (env Env) float64 {
+func (c call) Eval(env Env) float64 {
 	switch c.fn {
 	case "pow":
 		return math.Pow(c.args[0].Eval(env), c.args[1].Eval(env))
