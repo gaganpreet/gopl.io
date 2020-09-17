f53d47660d48c65dc5a3462be715e88cc570c8ef
Gaganpreet Arora <me@gaganpreet.in>
2020-09-17 21:01:47 +0200

    eval test

--- ch7/eval/eval_test.go
+++ ch7/eval/eval_test.go
@@ -1,4 +1,4 @@
-package expression
+package eval
 
 import (
 	"fmt"
@@ -14,6 +14,10 @@ func TestEval(t *testing.T) {
 	}{
 		{"sqrt(A/pi)", Env{"A": 87616, "pi": math.Pi}, "167"},
 		{"pow(x, 3) + pow(y, 3)", Env{"x": 12, "y":1}, "1729"},
+		{"pow(x, 3) + pow(y, 3)", Env{"x": 9, "y": 10}, "1729"},
+		{"5 / 9 * (F - 32)", Env{"F": -40}, "-40"},
+		{"5 / 9 * (F - 32)", Env{"F": 32}, "0"},
+		{"5 / 9 * (F - 32)", Env{"F": 212}, "100"},
 
 	}
 	var prevExpr string
--- ch7/eval/expr.go
+++ ch7/eval/expr.go
@@ -1,4 +1,4 @@
-package expression
+package eval
 
 import (
 	"fmt"
