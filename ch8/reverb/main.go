ab62346dbd5e198facff83aaa85f0dd4f807666e
Gaganpreet Arora <me@gaganpreet.in>
2020-11-18 21:46:31 +0100

    update netcat, and add example reverb

---
+++ ch8/reverb/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8002")
+	if err != nil {
+		log.Fatal(os.Stderr, "err: %s", err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(os.Stderr, "err: %s", err)
+		}
+		go handleConn(conn)
+	}
+
+}
+
+func handleConn(c net.Conn) {
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		echo(c, input.Text(), 1*time.Second)
+	}
+	// Note: Ignoring potential errors from input.Err()
+	c.Close()
+}
+
+func echo(c net.Conn, text string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(text))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", text)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(text))
+}
